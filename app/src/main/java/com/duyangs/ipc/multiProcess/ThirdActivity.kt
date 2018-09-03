5dafe86f12e9b5a640f9ea079c4ea7704913e033
duyang <duyangs1994@gmail.com>
2018-09-03 15:57:26 +0800

    Update multi process

--- app/src/main/java/com/duyangs/ipc/multiProcess/ThirdActivity.kt
+++ app/src/main/java/com/duyangs/ipc/multiProcess/ThirdActivity.kt
@@ -16,5 +16,6 @@ class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+        title = "Multi Process"
     }
 }
